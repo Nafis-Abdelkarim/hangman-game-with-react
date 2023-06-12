f88ae4efa369e69e1eb94413594cb6fd87028297
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-13 00:50:30 +0100

    Adding stateand passing data throw the components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Figure } from './components/Figure';
+import Header from './components/Header';
+import { Word } from './components/Word';
+import { WrongLetter } from './components/WrongLetter';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className='game-container'>
+      <Figure/>
+      <WrongLetter/>
+      <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
     </div>
+    </>
   );
 }
 
