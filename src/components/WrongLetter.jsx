d59eae9f28cc1b50cddb860018a50e64c1ce3404
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-13 11:39:03 +0100

    Update wrong letter

--- src/components/WrongLetter.jsx
+++ src/components/WrongLetter.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export const WrongLetter = () => {
+export const WrongLetter = ({WrongLetter}) => {
     return (
         <div className="wrong-letters-container">
-            <div id="wrong-letters"></div>
+            <div>
+                {WrongLetter.lenght > 0 && <p>Wrong</p>}
+                {WrongLetter
+                    .map((letter, i) => <span key={i}>{letter}</span>)
+                    .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null)
+                }
+            </div>
         </div>
     )
 }
