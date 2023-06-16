97bf509631ed5789c2e5c9d594c80d2182d7d9ba
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-06-16 02:20:24 +0100

    Adding lose noification message

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { checkWin } from '../helpers/helpers';
 
-export const Popup = () => {
+export const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable}) => {
+    let finalMessage = '';
+    let finalMessageRevealWord = '';
+    let playable = true;
+
+    if(checkWin(correctLetters, wrongLetters, selectedWord) === 'win' ){
+        finalMessage = 'Congratulations! You won! 😃'
+        playable = false
+    } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose'){
+        finalMessage = 'Unfortunately you lost. 😕';
+        finalMessageRevealWord = `...the wrold was ${selectedWord}`;
+        playable = false;
+    }
+
+
     return (
-    <div className="popup-container" id="popup-container">
+    <div className="popup-container" style={finalMessage != '' ? {display:'flex'} : {}}>
         <div className="popup">
-            <h2 id="final-message"></h2>
-            <h3 id="final-message-reveal-word"></h3>
+            <h2>{finalMessage}</h2>
+            <h3>{finalMessageRevealWord}</h3>
             <button id="play-button">Play Again</button>
         </div>
     </div>
